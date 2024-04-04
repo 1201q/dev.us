d12005f0f725ef02b507ac34b072dd816e804b52
hwang <1201q@naver.com>
2024-04-04 10:29:54 +0900

    [fix] 타입 오류

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -6,7 +6,7 @@ import {
   withUserTokenSSR,
 } from "next-firebase-auth";
 
-const Demo = ({ thing }) => {
+const Demo = ({ thing }: { thing: any }) => {
   console.log(thing);
   const user = useUser();
 
@@ -21,7 +21,7 @@ export const getServerSideProps = withUserTokenSSR({
   whenUnauthed: AuthAction.REDIRECT_TO_LOGIN,
 })(async ({ user }) => {
   const token = await user?.getIdToken();
-  const response = await fetch("http://localhost:3000/api/auth", {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth`, {
     method: "GET",
     headers: {
       authorization: token,
@@ -30,7 +30,7 @@ export const getServerSideProps = withUserTokenSSR({
   const data = await response.json();
   return {
     props: {
-      thing: data.user || null,
+      thing: data.user || { null: null },
     },
   };
 });
